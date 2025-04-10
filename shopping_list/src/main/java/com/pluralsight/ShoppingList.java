ca4637ec4cb652076764bc8142acf52b9dc02820
Brian Miller <Bmiller995@gmail.com>
2025-04-10 11:26:06 -0500

    Completed ShoppingList

---
+++ shopping_list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Bread");
+        System.out.println("Milk");
+        System.out.println("Sugar");
+        System.out.println("Ham");
+        System.out.println("Cookies");
+        System.out.println("Apples");
+        System.out.println("Watermelon");
+        System.out.println("Cereal");
+        System.out.println("Juice");
+        System.out.println("Water");
+
+    }
+}
